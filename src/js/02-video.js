225b8a86fa95368ecd4b49ca0cafad194cc14f11
Nazar Mosula <nmosula@gmail.com>
2022-12-21 21:34:12 +0200

    update video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const getSeconds = JSON.parse(
+        localStorage.getItem('videoplayer-current-time')
+);
+
+if (getSeconds != 'null') {
+    player.setCurrentTime(getSeconds);
+}
+
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+}, 1000));
