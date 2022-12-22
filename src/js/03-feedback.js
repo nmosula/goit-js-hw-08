a55eb4b35b4199fba1bcb6fc7668fabb3c82dc11
Nazar Mosula <nmosula@gmail.com>
2022-12-22 13:58:41 +0200

    Update 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from 'lodash.throttle';
 
+const frm = document.querySelector(".feedback-form");
+const frmEmail = document.querySelector("input[name='email']");
+const frmMessage = document.querySelector("textarea[name='message']");
+
+const objFormData = {};
+
+frm.addEventListener('input', throttle(onInput, 500));
+frm.addEventListener('submit', onSubmit);
+
+getFormValue();
+
+function onInput() {
+    objFormData[frmEmail.name] = frmEmail.value;
+    objFormData[frmMessage.name] = frmMessage.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(objFormData));
+}
+
+
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    const returnFormElements = {};
+
+    const frmElements = evt.currentTarget.elements;
+
+    for (const el of frmElements) {
+        returnFormElements[el.name] = el.value
+    }
+    console.log(returnFormElements);
+
+    frm.reset();
+    localStorage.removeItem("feedback-form-state");
+}
+
+function getFormValue() {
+    try {
+        const getValues = JSON.parse(
+            localStorage.getItem('feedback-form-state')
+        );
+
+        if (getValues != 'null') {
+            frmEmail.value = getValues.email;
+            frmMessage.value = getValues.message;
+        }
+    }
+    catch (e) {
+        // console.log(e);
+    }
+
+}
